4a334e5b7c0b755889902b50368a009785fb6789
Favio Terrones <favioalonso156@gmail.com>
2023-07-05 23:15:15 -0500

    Revert "agregando estadisticas"
    This reverts commit 70393a7ffd981a18e42136c47235b76e65bb4a52.

--- src/components/administrador/RevisarEstadisticaReserva.jsx
+++ src/components/administrador/RevisarEstadisticaReserva.jsx
@@ -1,29 +1,8 @@
-import { Bar } from 'react-chartjs-2';
-
-
 
 const RevisarEstadisticaReserva = () => {
-
-  const data = {
-    labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'],
-    datasets: [
-      {
-        label: 'Ventas',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: 'rgba(75,192,192,1)',
-      },
-    ],
-  };
-
-  const options = {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  };
-
-  return <Bar data={data} options={options} />;
+  return (
+    <div>RevisarEstadisticaReserva</div>
+  )
 }
 
 export default RevisarEstadisticaReserva
