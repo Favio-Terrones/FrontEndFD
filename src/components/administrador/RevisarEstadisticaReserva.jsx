9c1a4ca8558e65ea8eee3ae6d32b5c4cceba5f6b
Favio Terrones <favioalonso156@gmail.com>
2023-07-05 23:19:18 -0500

    Corrigiendo la cagada que hizo doge

--- src/components/administrador/RevisarEstadisticaReserva.jsx
+++ src/components/administrador/RevisarEstadisticaReserva.jsx
@@ -1,8 +1,30 @@
+import { Bar } from 'react-chartjs-2';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
+
 
 const RevisarEstadisticaReserva = () => {
-  return (
-    <div>RevisarEstadisticaReserva</div>
-  )
+  const data = {
+    labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'],
+    datasets: [
+      {
+        label: 'Ventas',
+        data: [12, 19, 3, 5, 2, 3], // Valores de las barras
+        backgroundColor: 'rgba(75,192,192,0.6)', // Color de las barras
+      },
+    ],
+  };
+
+  // Opciones de configuración del gráfico
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return <Bar data={data} options={options} />;
 }
 
 export default RevisarEstadisticaReserva
